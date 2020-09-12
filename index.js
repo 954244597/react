55770b0d99d1efe23c71b8dc2614be1241232a4d
陈铭 <954244597@qq.com>
2020-09-12 21:14:51 +0800

    commit

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ http.createServer(async (req, res) => {
     if (req.method === 'POST' && req.url === '/') {
         const data = await resolvePost(req);
         const projectDir = path.resolve(__dirname,`./${data.repository.name}`)
+        console.log('CWM',projectDir)
         deleteFolderRecursive(projectDir)
 
         // 拉取仓库最新代码
